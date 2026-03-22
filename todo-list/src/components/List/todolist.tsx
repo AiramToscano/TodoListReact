98183b6ce0801fa039fb2f39624eeab57e67ebdf
Airam Toscano <airamlobato@fiteclabs.org.br>
2026-03-22 17:50:25 -0300

    feat: testando merge

--- todo-list/src/components/List/todolist.tsx
+++ todo-list/src/components/List/todolist.tsx
@@ -1,96 +1,96 @@
-import { useEffect, useState } from "react"
-import type { List } from "../../view/List"
-import axios from "axios"
-import { useQuery } from "@tanstack/react-query"
-
+import { useEffect, useState } from "react";
+import type { List } from "../../view/List";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 interface ListProps {
-  data: List [],
-  setValueList: React.Dispatch<React.SetStateAction<List[]>>
+  data: List[];
+  setValueList: React.Dispatch<React.SetStateAction<List[]>>;
 }
 
-
-function TodoList({data, setValueList}:ListProps ) {
-  const [idList, setIdList] = useState('')
-  const [value, setValue] = useState('')
-
+function TodoList({ data, setValueList }: ListProps) {
+  const [idList, setIdList] = useState("");
+  const [value, setValue] = useState("");
 
   const fetchTodos = async (): Promise<any> => {
-  const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=0`)
-  return response.data
-}
+    const response = await axios.get(
+      `https://rickandmortyapi.com/api/character/?page=0`,
+    );
+    return response.data;
+  };
 
   const api = useQuery({
-    queryKey: ['todos'],
+    queryKey: ["todos"],
     queryFn: () => fetchTodos(),
-  })
+  });
 
   const handleClickEdit = (id: string, name: string) => {
-    setIdList(id)
-    setValue(name)
-  }
+    setIdList(id);
+    setValue(name);
+  };
 
-    const handleClickDelete = (id: string) => {
-
-     const newData = data.filter((e) => e.id !== id)
-   setValueList(newData)
-  }
+  const handleClickDelete = (id: string) => {
+    const newData = data.filter((e) => e.id !== id);
+    setValueList(newData);
+  };
 
   const handleInputEnter = () => {
-     const newData = data.map((e) => {
-       return {
-      ...e,
-      name: e.id === idList ? value : e.name
-      }
-    })
-   setValueList(newData)
-   setIdList('')
-       
-        
-  }
+    const newData = data.map((e) => {
+      return {
+        ...e,
+        name: e.id === idList ? value : e.name,
+      };
+    });
+    setValueList(newData);
+    setIdList("");
+  };
 
-    useEffect( () => {
+  useEffect(() => {
+    const list = JSON.parse(sessionStorage.getItem("list") || "null");
 
- const list = JSON.parse(sessionStorage.getItem("list") || "null");
-
-  if(list && list.length > 0) setValueList(list)
-
+    if (list && list.length > 0) setValueList(list);
   }, []);
 
-
   return (
-   
     <section>
-    <ul>
-  {data.map(list => (
-    <li key={list.id} className="main-todo-list">
-      {list.id === idList ? (
-        <input  onKeyDown={e => {
-    if (e.key === "Enter") {
-      handleInputEnter();
-    }
-  }}  onChange={(e) => setValue(e.target.value) } value={value} />
-      ) : (
-        <span>{list.name}</span>
-      )}
-      <section>
-      <button type="button" onClick={() => handleClickEdit(list.id, list.name)}>
-        edit
-      </button>
-        <button type="button" onClick={() => handleClickDelete(list.id)}>
-        delete
-      </button>
-      </section>
-    </li>
-  ))}
-</ul>
-  {api.data?.results.map((e) => (
-    <div>
-      <span>{e.name}</span>
-    </div>
-  ))}
+      <h1> não é para aparecer em main</h1>
+      <ul>
+        {data.map((list) => (
+          <li key={list.id} className="main-todo-list">
+            {list.id === idList ? (
+              <input
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleInputEnter();
+                  }
+                }}
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+              />
+            ) : (
+              <span>{list.name}</span>
+            )}
+            <section>
+              <button
+                type="button"
+                onClick={() => handleClickEdit(list.id, list.name)}
+              >
+                edit
+              </button>
+              <button type="button" onClick={() => handleClickDelete(list.id)}>
+                delete
+              </button>
+            </section>
+          </li>
+        ))}
+      </ul>
+      {api.data?.results.map((e) => (
+        <div>
+          <span>{e.name}</span>
+        </div>
+      ))}
     </section>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
